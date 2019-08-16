28e1d1f87e3f06a3d885fe11bb983bc56a72c8db
Nicolas Baumgardt <nicolas@baumgardt.ch>
2019-08-16 02:10:24 +0200

    Update A3000M_LED_Install.sh

--- A3000M_LED_Install.sh
+++ A3000M_LED_Install.sh
@@ -2,21 +2,33 @@
 echo "*******************************************"
 echo "* AMIGA 3000 MINI LED INSTALLATION SCRIPT *"
 echo "*******************************************"
-echo " "
 if [ "$(whoami)" != "root" ]; then
+	echo " "
 	echo "Sorry, you can't run this script with normal user privileges."
         echo "Call the script with super user privileges like this:"
         echo "sudo ./A3000M_LED_Install.sh"
 	exit 1
 fi
+echo " "
 echo " -> Which Raspberry Pi model do you use (1, 2, 3 or 4) ?"
 read model
+if [ "$model" = "1" ] || [ "$model" = "2" ] || [ "$model" = "3" ] || [ "$model" = "4" ]
+    then
+    	echo " "
+    	echo " -> Going to update your operating system ..."
+	echo " "
+else
+	echo " "
+	echo "Script aborted. You need to provide a valid Raspberry Pi model (major version) -> 1, 2, 3 or 4."
+	exit 1
+fi
 apt-get purge wiringpi
 apt-get install git-core
 apt-get update
 apt-get upgrade
 if [ "$model" = "1" ] || [ "$model" = "2" ] || [ "$model" = "3" ]
     then
+    	echo " "
 	echo " -> Going to install the WiringPi library ..."
 	echo " "
 	mkdir /tmp/WiringPi
@@ -25,12 +37,14 @@ if [ "$model" = "1" ] || [ "$model" = "2" ] || [ "$model" = "3" ]
 	./build
 elif [ "$model" = "4" ]
     then
+    	echo " "
 	echo " -> Going to install the WiringPi library ..."
 	echo " "
 	cd /tmp
 	wget https://project-downloads.drogon.net/wiringpi-latest.deb
 	dpkg -i wiringpi-latest.deb
 else
+	echo " "
 	echo "Script aborted. You need to provide a valid Raspberry Pi model (major version) -> 1, 2, 3 or 4."
 	exit 1
 fi
@@ -48,7 +62,7 @@ systemctl daemon-reload
 systemctl enable hddledPi.service
 systemctl enable netledPi.service
 echo " "
-echo " -> You need to reboot your Raspberry Pi. Reboot now (Y=yes, n=no) ?"
+echo " -> You need to restart your Raspberry Pi to make the LEDs work. Reboot now (Y=yes, n=no) ?"
 read answer
 if [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [ "$answer" = "j" ] || [ "$answer" = "J" ]
     then
